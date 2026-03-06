1403ca6605de4fa8f5e00da23f29395b5a957998
maitamgk <maitamit062005@gmail.com>
2026-03-06 23:47:00 +0700

    feat: add constants barrel export

---
+++ app/lib/constants/index.ts
@@ -0,0 +1,7 @@
+﻿export * from './riskLevels'
+export * from './scamCategories'
+export * from './tiers'
+export * from './navigation'
+export * from './seo'
+export * from './errorMessages'
+export * from './regex'feat: add constants barrel export file
