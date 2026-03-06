a920b1f26c4e8f23fc754c0ca6edfabf6904a820
maitamgk <maitamit062005@gmail.com>
2026-03-06 13:42:05 +0700

    feat: add hooks barrel export file

---
+++ app/hooks/index.ts
@@ -0,0 +1,29 @@
+/**
+ * Custom Hooks - Barrel Export
+ * Re-export all custom hooks for easy imports
+ */
+
+export { useDebounce } from './useDebounce'
+export { useThrottle } from './useThrottle'
+export { useLocalStorage } from './useLocalStorage'
+export { useSessionStorage } from './useSessionStorage'
+export { useDarkMode } from './useDarkMode'
+export { useMediaQuery, useIsMobile, useIsTablet, useIsDesktop } from './useMediaQuery'
+export { useOnlineStatus } from './useOnlineStatus'
+export { useClipboard } from './useClipboard'
+export { useToggle } from './useToggle'
+export { useCounter } from './useCounter'
+export { usePrevious } from './usePrevious'
+export { useDocumentTitle } from './useDocumentTitle'
+export { useKeyPress } from './useKeyPress'
+export { useClickOutside } from './useClickOutside'
+export { useWindowSize } from './useWindowSize'
+export { useScrollPosition } from './useScrollPosition'
+export { useHover } from './useHover'
+export { useFetch } from './useFetch'
+export { useAsync } from './useAsync'
+export { useInterval } from './useInterval'
+export { useTimeout } from './useTimeout'
+export { useLockBodyScroll } from './useLockBodyScroll'
+export { useIntersectionObserver } from './useIntersectionObserver'
+export { useGeolocation } from './useGeolocation'
